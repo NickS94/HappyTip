ef6b80004249b257b50af3047a60ae589ddc20bc
NickS94 <stauropoulosn@gmail.com>
2024-06-17 09:42:59 +0200

    Initial Commit

---
+++ studenttestapp/studenttestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  studenttestappApp.swift
+//  studenttestapp
+//
+//  Created by Nikos Stauropoulos on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct studenttestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
